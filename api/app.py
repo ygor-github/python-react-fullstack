296f87df6d40bffddb3d8adaea57bab489ef06e3
Ygor Alejandro <ygorhernandezbr@gmail.com>
2025-10-04 22:09:03 -0300

    feat: Connect Flask to PostgreSQL and create Word model
    Integrates Flask-SQLAlchemy and Flask-Migrate.
    
    Defines the 'Word' model with an ID, text, and timestamp field.
    
    Applies the first migration to create the 'words' table in the PostgreSQL container.

--- api/app.py
+++ api/app.py
@@ -1,9 +1,27 @@
 from flask_cors import CORS
 from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
 import datetime
+import os
+
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Word(db.Model):
+    __tablename__ = 'words'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(120), unique=True, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    def __repr__(self):
+        return f'<Word {self.text}>'
+
 
 @app.route('/api/time')
 def time_now():
@@ -11,7 +29,3 @@ def time_now():
   current_time = datetime.datetime.now()
   formated_time = current_time.strftime('%Y-%m-%d %H:%M:%S')
   return jsonify({'time':formated_time})
-
-if __name__ == '__main__':
-  app.run(debug=True)
-  
